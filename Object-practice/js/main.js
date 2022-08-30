988a2943503f166c39ec2cac6991e4a2a730f989
Muthurajchella- <muthuraj.chella@gmail.com>
2022-08-30 15:04:18 +0530

    Object

--- Object-practice/js/main.js
+++ Object-practice/js/main.js
@@ -1,42 +1,192 @@
 /*
 let myBike = {
 	company : "Bajaj",
-	model : "Pulsar 220F",
-	color : "Black and Red",
-	bhp :  "8500rpm",
-	engine : "Bs VI",	
+	model	: "Pulsar 220F",
+	color 	: "Black and Red",
+	bhp	 	:  "8500rpm",
+	engine	: "Bs VI",	
 }
 	console.log(myBike.company +" "+ myBike.model +" "+ myBike.color +" "+ myBike.bhp +" "+ myBike.engine);
 	document.write(myBike.company +" "+ myBike.model +" "+ myBike.color +" "+ myBike.bhp +" "+ myBike.engine);
-	
 	*/
 	
 
-let marriage = "By The Grace Of God Muruga Marriage Fixed by Our family ";
+
+let marriage = "By The Grace Of God Muruga!!!. <br> Marriage Fixed by Our family ";
 	
 	console.log(marriage);
-	document.write(marriage);
+	document.write(marriage + "<br>");
 
  
  let boy = {
-	 name : "Raj",
-	 age  : 25+ "yearas old",
-	 job : "Subinspector Of police Tamilnadu",
-	 education : "B.E Mechanical",
+	 name 		: "Raj",
+	 age  		: 25+ "yearas old",
+	 job 		: "Subinspector Of police Tamilnadu",
+	 education 	: "B.E Mechanical",
 	 bloodgroup : "B+ve",
-	 color : "light-white"
+	 color 		: "light-white"
  }
 	console.log(boy.name + " " +boy.age + " " +boy.job + " " +boy.education + " " +boy.bloodgroup + " " +boy.color);
-	document.write(boy.name + " " +boy.age + " " +boy.job + " " +boy.education + " " +boy.bloodgroup + " " +boy.color);
+	document.write("<br>"+ boy.name + " <br>" +boy.age + "<br> " +boy.job + "<br>" +boy.education + "<br>" +boy.bloodgroup + "<br>" +boy.color +"<br><br>");
 
 let girl = {
-	 name : "Veera",
-	 age  : 21+ "yearas old",
-	 job : "Doctor",
-	 education : "MS, MBBS",
+	 name 		: "Veera",
+	 age  		: 21+ "yearas old",
+	 job 		: "Doctor",
+	 education 	: "MS, MBBS",
 	 bloodgroup : "O+ve",
-	 color : "Yellow-White"
+	 color 		: "Yellow-White"
 }
 	console.log(girl.name + " " +girl.age + " " +girl.job + " " +girl.education + " " +girl.bloodgroup + " " +girl.color);
-	document.write(girl.name + " " +girl.age + " " +girl.job + " " +girl.education + " " +girl.bloodgroup + " " +girl.color);
+	document.write(girl.name + "<br> " +girl.age + "<br> " +girl.job + "<br> " +girl.education + "<br> " +girl.bloodgroup + "<br> " +girl.color);
+
+
+		
+/*
+let waterTank = {
+	 company 	: "AQUA FLEX",
+	 layerType  : "Triple Layer",
+	 material 	: "Plastic",
+	 color 		: "White",
+	 capacity	: "2000 litre",
+	 shape 		: "Round",
+	 
+}
+	console.log(waterTank.company + " " +waterTank. layerType + " " +waterTank.material + " " +waterTank.color + " " +waterTank.bloodgroup + " " +waterTank. capacity + " " +waterTank.shape);
+	
+
+let pvcPipe = {
+	 pvc: "Polyvinyl Chloride ",
+	 size  		: 15 +  "mm(1/2 inch)",
+	 length 	: 3+"m",
+	 usage 		: "Plumbing",
+	 madeIn 	: "India",
+	 color 		: "white"
+}
+	console.log(pvcPipe.name + " " +pvcPipe.pvc + " " +pvcPipe.size + " " +pvcPipe.length + " " +pvcPipe.usage + " " +pvcPipe.color);
+	
+
+let india = {
+	 capital 	: "Delhi",
+	 pm  		:  "Modi",
+	 president 	: "Droupadi Murmu",
+	 population : 138 + "crores",
+}
+	console.log(india.capital + " " +india. pm + " " +india. president  + " " +india.population);
+
+	
+
+
+let tamilNadu = {
+	 capital 	: "Chennai",
+	 covernor 	: "R.N.Ravi",
+	 cm  		:  "Stalin",
+	 language 	: "Tamil",
+	 population : 6.79 + "crores",
+}
+	console.log(tamilNadu.capital + " " +tamilNadu.covernor + " " +tamilNadu.cm  + " " +tamilNadu.population+ " " + tamilNadu.language);
+		
+	
+	
+	
+let abbriviation = {
+	 isro 	: "Indian Space Research Organisation.",
+	 nasa	: "National Aeronautics and Space Administration",
+	 upsc 	: "UNION PUBLIC SERVICE COMMISSION",
+	 mbbs	: "Bachelor of Medicine, Bachelor of Surgery",
+	 b.e 	: "Bachelor of Engineering",
+	 ips 	: " Indian Police Service."
+	 
+}
+console.log(abbriviation .isro+ " " +abbriviation .nasa + " " +abbriviation .upsc + " " +abbriviation .mbbs + " " +abbriviation .b.e + " " +abbriviation . Indian Police Service.);
+		
+	
+	
+	
+let shirts = {
+	 
+	 brand 	: "Otto",
+	 size 	: "36",
+	 model 	: "kurtha",
+	 color 	: "Orenge",
+	 rate 	: 1001+"rupees"
+}
+	console.log(shirts.brand  + " " +shirts.size + " " +shirts.model + " " +shirts.color + " " +shirts.rate);
+	
+
+
+let shoes = {
+	 brand 		: "Puma",
+	 model  	: "ProAdapt AlphaCat Disc",
+	 playerType: "Men",
+	 shoesType	: "Spikeless",
+	 size	 	: "10 1/2",
+	 color 		: "Puma White / High Rise"
+}
+	console.log(shoes.brand  + " " +shoes.model  + " " +shoes.player Type + " " +shoes.shoes Type + " " +shoes. size + " " +shoes.color);
+
+
+let sunglasses = {
+	 brand 		: "Ray Ban",
+	 model  	: "Aviator Sunglass",
+	 playerType	: "Men",
+	 glassType	: "SKU",
+	 madeIn		: "India",
+	 color 		: "Brown"
+}
+	console.log(sunglasses.brand  + " " +sunglasses.model  + " " +sunglasses.Player Type + " " +sunglasses.glassType + " " +sunglasses. Size + " " +sunglasses.color);
+
+	
+	
+let bike = {
+    model			: "fzsv-3",
+    fueltype		: "petrol",
+    fuelcapacity	: "13",
+    mileage			: "45",
+    vehiclecc		: "149"
+	color			: "black"
+}
+	console.log(bike.model  + " " +bike.fueltype  + " " +bike.fuelcapacity + " " +bike.mileage + " " +bike. vehiclecc + " " +bike.color);
+	
+	
+	
+let car={
+    model			: "swift-dzire",
+    fueltype		: "disel",
+    fuelcapacity	: "45",
+    mileage			: "18kmpl",
+    colour			: "brown"
+}
+	console.log(car.model  + " " +car.fueltype  + " " +car.fuelcapacity + " " +car.mileage + " " +car.color);
+	
+	
+	
+let laptop={
+    model		: "avita pura",
+    Ram			: "4gp",
+    harddisk	: "128gp",
+    price		: "23000",
+    colour		: "ashgrey",
+	
+
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
