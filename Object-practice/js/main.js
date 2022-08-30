8d3d48e87b8d1b226b6e515bf228e5b8dc3e0225
Muthurajchella- <muthuraj.chella@gmail.com>
2022-08-30 11:11:20 +0530

    Object

---
+++ Object-practice/js/main.js
@@ -0,0 +1,42 @@
+/*
+let myBike = {
+	company : "Bajaj",
+	model : "Pulsar 220F",
+	color : "Black and Red",
+	bhp :  "8500rpm",
+	engine : "Bs VI",	
+}
+	console.log(myBike.company +" "+ myBike.model +" "+ myBike.color +" "+ myBike.bhp +" "+ myBike.engine);
+	document.write(myBike.company +" "+ myBike.model +" "+ myBike.color +" "+ myBike.bhp +" "+ myBike.engine);
+	
+	*/
+	
+
+let marriage = "By The Grace Of God Muruga Marriage Fixed by Our family ";
+	
+	console.log(marriage);
+	document.write(marriage);
+
+ 
+ let boy = {
+	 name : "Raj",
+	 age  : 25+ "yearas old",
+	 job : "Subinspector Of police Tamilnadu",
+	 education : "B.E Mechanical",
+	 bloodgroup : "B+ve",
+	 color : "light-white"
+ }
+	console.log(boy.name + " " +boy.age + " " +boy.job + " " +boy.education + " " +boy.bloodgroup + " " +boy.color);
+	document.write(boy.name + " " +boy.age + " " +boy.job + " " +boy.education + " " +boy.bloodgroup + " " +boy.color);
+
+let girl = {
+	 name : "Veera",
+	 age  : 21+ "yearas old",
+	 job : "Doctor",
+	 education : "MS, MBBS",
+	 bloodgroup : "O+ve",
+	 color : "Yellow-White"
+}
+	console.log(girl.name + " " +girl.age + " " +girl.job + " " +girl.education + " " +girl.bloodgroup + " " +girl.color);
+	document.write(girl.name + " " +girl.age + " " +girl.job + " " +girl.education + " " +girl.bloodgroup + " " +girl.color);
+	
--- Use conditions/js/script.js
+++ Use conditions/js/script.js
@@ -1,32 +1,10 @@
 let a = parseInt(prompt("1st value"));
 let b = parseInt(prompt("2nd value"));
 let c = parseInt(prompt("3rd value"));
-if (a>b && a>c){
-	if (b>c){
-		console.log(c,b,a);
-	}
-}
-	else
-		(c>b);
-	console.log(b,c,a);
-	
-if (b>c && b>a){
-	if (a>c){
-		console.log(c,a,b);
-	}
-}
-	else
-		(c>a);
-	console.log(a,c,b);
-	
-if (c>a && c>b){
-	if (b>a){
-		console.log(a,b,c);
-	}
-}
-	else
-		(a>b);
-	console.log(b,a,c);
-	
-					
+if(a>b && a>c)
+	alert(a,"greater no");
+		else if(b>c && b>a)
+			alert (b,"greater no");
+				else(c>a && c>b);
+					alert(c,"greater no");
 		
