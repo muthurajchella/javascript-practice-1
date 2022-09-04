b85cd65cfc64b8ea9c5993b7de92bee957bfe80c
Muthurajchella- <muthuraj.chella@gmail.com>
2022-09-04 23:56:49 +0530

    practice

---
+++ Use conditions/js/main.js
@@ -0,0 +1,52 @@
+/*let a = parseInt(prompt("give a number"));
+if(0<a){
+	console.log("This number is positive");
+}
+	else{
+		console.log("This number is negative");
+	}
+	
+let b = parseInt(prompt("give anumber"));
+if(b % 2==1){
+	console.log("Add Numbers");
+}
+
+*/
+
+
+var age = 21;
+var gpa = 7.5;
+console.log(age);
+console.log(gpa);
+
+var octNum = 0255;
+var hexNum = 0xff;
+console.log(octNum);
+console.log(hexNum);
+
+var amount = 1.1e6;
+console.log(amount);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+var name = "MUTHURAJ.C";
+var profession = "Software Devloper";
+var qulifi = "B.E Mechanical";
+
+console.log(name);
+console.log(profession);
+console.log(qulifi);
+
+var a ;
+a = 10;
+console.log(a);
+console.log(typeof a);
+console.log(typeof qulifi);
+console.log(typeof amount);
+
+var b;
+console.log(typeof b);
+
+var c = [];
+console.log(typeof c);
