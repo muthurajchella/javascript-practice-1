1f49a458fc3e5af67c097353728b6c9e165896a8
Muthurajchella- <muthuraj.chella@gmail.com>
2022-09-16 16:55:41 +0530

    new

--- sum/js/script.js
+++ sum/js/script.js
@@ -1,4 +1,42 @@
-let a = parseInt(prompt("give no1 :"));
-let b = parseInt(prompt("give no1 :"));
-let c = a + b;
-console.log(c);
+
+//BODMAS rules
+/*
+var a = 3;
+var b1 = 4;
+var b2 = Math.pow(5, 2);
+var b = b1 * b2;
+var c1 = 6;
+var c2 = 7;
+var c3 = 8;
+var c = c1+c2-c3;
+ 
+var sovl = a * (b/c);
+console.log('Result:', sovl);
+
+var d = [65+85+70+190+105];
+d /= 5;
+console.log(d);
+
+var d1 = parseInt(prompt("Give numbers:"));
+var d2 = parseInt(prompt("Sum of those numbers:"));
+var d = d2 / d1;
+console.log(d);
+*/
+
+var x = 5;
+console.log(x);
+y =++ x;
+console.log(y);
+console.log(x);
+
+var example = 5;
+console.log(example);
+z = example++;
+console.log(example);
+console.log(z);
+
+let m = 2;
+console.log(m);
+let r = ++m;
+console.log(m);
+
